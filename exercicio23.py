8d4fdbb42f0a9de9875a0922d92d6eda8eebc45c
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-10-16 05:39:26 -0300

    Update exercicio23.py

--- exercicio23.py
+++ exercicio23.py
@@ -3,10 +3,14 @@ unidade = numero // 1 % 10
 dezena = numero // 10 % 10
 centena = numero // 100 % 10
 milhar = numero // 1000 % 10
-print('unidade: {}'.format(unidade))
+print('unidade: {}{}{}'.format('\033[35m', unidade, '\033[m'))
 print('dezena: {}'.format(dezena))
 print('centena: {}'.format(centena))
 print('milhar: {}'.format(milhar))
+
+
+
+
 
 
 
