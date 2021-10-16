08f3d6a7faed814146ea10c4dce623518d1a2da4
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-10-16 05:42:13 -0300

    Update exercicio27.py

--- exercicio27.py
+++ exercicio27.py
@@ -1,7 +1,10 @@
 name = str(input('Digite o seu nome: ')).strip()
-print('Muito prazer em te conhecer {} !'.format(name))
+print('Muito prazer em te conhecer {}{}{} !'.format('\033[31m', name, '\033[m'))
 nome = name.split()
-print('O seu primeiro nome é: {}'.format(nome[0]))
+print('O seu primeiro nome é: {}{}{}'.format('\033[33m', nome[0], '\033[m'))
 print('O seu último nome é: {}'.format(nome[len(nome) - 1]))
+
+
+
 
 
