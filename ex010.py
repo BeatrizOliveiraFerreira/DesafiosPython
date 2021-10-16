2a09aa098ef741d7c678116ab339d7704e142ad5
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-10-16 05:32:00 -0300

    Update ex010.py

--- ex010.py
+++ ex010.py
@@ -1,5 +1,8 @@
 produto = float(input('Qual o preço do produto? '))
 desconto = produto - (produto * 5 / 100)
-print('O produto com 5% de desconto é {}'.format(desconto))
+print('O produto com 5% de desconto é {}{}{}'.format('\033[34;41m', desconto, '\033[m'))
+
+
+
 
 
