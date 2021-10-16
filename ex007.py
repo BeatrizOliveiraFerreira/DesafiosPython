7153ccc941c206fca61297645d4b3e868ca03921
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-10-16 05:30:19 -0300

    Update ex007.py

--- ex007.py
+++ ex007.py
@@ -1,4 +1,6 @@
 numero = int(input('Digite um número para fazer a tabuada: '))
 tabuada = numero * 1, numero * 2, numero * 3, numero * 4, numero * 5, numero * 6, numero * 7, numero * 8, numero * 9, numero * 10
-print(tabuada)
+print('\033[41m', tabuada, '\033[m')
+
+
 
