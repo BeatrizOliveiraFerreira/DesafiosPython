b6d4c82145caff196b4729e9de44ee9f56fabbf6
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-10-16 05:41:31 -0300

    Update exercicio26.py

--- exercicio26.py
+++ exercicio26.py
@@ -1,6 +1,6 @@
 frase = str(input('Digite uma frase: ')).upper().strip()
 print('A letra "A" aparece {} vezes'.format(frase.count('A')))
-print(frase.find('A')+1)
+print('\033[45m', frase.find('A')+1, '\033[m')
 print(frase.rfind('A')+1)
 
 
