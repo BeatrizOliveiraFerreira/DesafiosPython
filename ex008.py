01b1dc2d2c391172aa248e86068898c1a71052a4
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-10-16 05:31:00 -0300

    Update ex008.py

--- ex008.py
+++ ex008.py
@@ -1,5 +1,8 @@
 dinheiro = float(input('Digite o quanto você tem: '))
 dolar = 5.34
-print('Você pode comprar {:.2f} dólares'.format(dinheiro / dolar))
+print('Você pode comprar {}{:.2f}{} dólares'.format('\033[44m', dinheiro / dolar, '\033[m'))
+
+
+
 
 
