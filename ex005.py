25dbdee08576b3729f8eabad79a1bd71a92fb8e4
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-10-16 05:29:18 -0300

    Update ex005.py

--- ex005.py
+++ ex005.py
@@ -1,4 +1,5 @@
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 media = (nota1 + nota2) / 2
-print('A média desse aluno foi: {}'.format(media))
+print('A média desse aluno foi: {}{}{}'.format('\033[34m', media, '\033[m'))
+
