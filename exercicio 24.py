d8bb52e1358bbcf66f8e4e9c148003e7f1645872
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-10-16 05:39:59 -0300

    Update exercicio 24.py

--- exercicio 24.py
+++ exercicio 24.py
@@ -1,5 +1,9 @@
 cidade = str(input('Digite a cidade: ')).strip()
-print('A cidade começa com Santo? {}'.format(cidade[:5].upper() == 'SANTO'))
+print('A cidade começa com Santo? {}{}{}'.format('\033[36m', cidade[:5].upper() == 'SANTO', '\033[m'))
+
+
+
+
 
 
 
