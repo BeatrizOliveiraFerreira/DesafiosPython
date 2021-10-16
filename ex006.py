6098123cc64c34ceb816a7f4627fe29f6f950932
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-10-16 05:29:48 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -2,6 +2,7 @@ metros = int(input('Digite um valor: '))
 metros1 = (metros) * 10 ** 0
 centimetros = (metros) * 10 ** -2
 milimetros = (metros) * 10 ** -3
-print(
-    'Esse número em metros é = {}m\nEm centímetros é = {}cm\nEm milímetros = {}mm'.format(metros1, centimetros, milimetros))
+print('Esse número em metros é = {}{}{}m\nEm centímetros é = {}cm\nEm milímetros = {}mm'.format('\033[31m', metros1, centimetros, milimetros, '\033[m'))
+
+
 
