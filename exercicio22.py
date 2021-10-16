262ebea7ec69e2928b02401d07f20b55019dc2b6
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-10-16 05:38:34 -0300

    Update exercicio22.py

--- exercicio22.py
+++ exercicio22.py
@@ -3,7 +3,16 @@ print('Nome em Maiúsculo: {}'.format(nome.upper()))
 print('Nome em Minúsculo: {}'.format(nome.lower()))
 print('Nome total: {}'.format(len(nome) - nome.count(' ')))
 nome_com_split = nome.split()
-print('O primeiro nome tem {} letras'.format(len(nome_com_split[0])))
+print('O primeiro nome tem {}{}{} letras'.format('\033[31m', len(nome_com_split[0]), '\033[m'))
+
+
+
+
+
+
+
+
+
 
 
 
