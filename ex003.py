882b9928be0ce22da1ebefa41ba56217c92522fe
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-10-16 05:27:55 -0300

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -1,6 +1,7 @@
 n1 = int(input('Digite um número e direi o antecessor e o sucessor: '))
 antes = n1 - 1
 depois = n1 + 1
-print('Antes do número {} vem o {} e depois do {} vem o {}'.format(n1, antes, n1, depois))
+print('Antes do número {}{} vem o {} e depois do {} vem o {}{}'.format('\033[43m', n1, antes, n1, depois, '\033[m'))
+
 
 
