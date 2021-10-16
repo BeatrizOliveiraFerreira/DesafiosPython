127bed1cebd07f97c7203d5ac5a8726ee5eb4432
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-10-16 05:31:33 -0300

    Update ex009.py

--- ex009.py
+++ ex009.py
@@ -2,4 +2,4 @@ largura = float(input('Qual a largura da parede? '))
 altura = float(input('Qual a altura da parede? '))
 area = largura * altura
 tinta = (area) / 2
-print(tinta)
+print('\033[33m', tinta, '\033[m')
