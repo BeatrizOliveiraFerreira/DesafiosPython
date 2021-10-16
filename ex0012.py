7f510c3a2f7b0c3f69be6d9021d70f58e3728cc4
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-10-16 05:33:10 -0300

    Update ex0012.py

--- ex0012.py
+++ ex0012.py
@@ -3,7 +3,11 @@ quilometros = float(input('Quantos Km rodados? '))
 precoDias = 60.0
 precoQuilometro = 0.15
 resultado = (dias * precoDias) + (quilometros * precoQuilometro)
-print('O preço a pagar é: R${:.2f}'.format(resultado))
+print('O preço a pagar é: R${}{:.2f}{}'.format('\033[33m', resultado, '\033[m'))
+
+
+
+
 
 
 
